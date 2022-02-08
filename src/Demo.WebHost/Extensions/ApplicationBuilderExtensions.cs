92bf245259c0b3fe2c90c6c92fc899433650e76d
Minh Hiền <minhhiena712@gmail.com>
2022-02-08 17:09:53 +0700

    add extension for startup class to register + configure service + build module

---
+++ src/Demo.WebHost/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
+using System;
+
+namespace Demo.WebHost.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseCustomizedMvc(this IApplicationBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                //routes.Routes.Add(new UrlSlugRoute(routes.DefaultHandler));
+
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+            return app;
+        }
+
+        public static IApplicationBuilder UseCustomizedStaticFiles(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    OnPrepareResponse = (context) =>
+                    {
+                        var headers = context.Context.Response.GetTypedHeaders();
+                        headers.CacheControl = new CacheControlHeaderValue
+                        {
+                            NoCache = true,
+                            NoStore = true,
+                            MaxAge = TimeSpan.FromDays(-1)
+                        };
+                    }
+                });
+            }
+            else
+            {
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    OnPrepareResponse = (context) =>
+                    {
+                        var headers = context.Context.Response.GetTypedHeaders();
+                        headers.CacheControl = new CacheControlHeaderValue
+                        {
+                            Public = true,
+                            MaxAge = TimeSpan.FromDays(60)
+                        };
+                    }
+                });
+            }
+
+            return app;
+        }
+
+    }
+}
---
+++ src/Demo.WebHost/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,120 @@
+﻿using Demo.Infrastructure;
+using Demo.Infrastructure.Modules;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Demo.WebHost.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        private static readonly IModuleConfigurationManager _modulesConfig = new ModuleConfigurationManager();
+
+        public static IServiceCollection AddModules(this IServiceCollection services, string contentRootPath)
+        {
+            foreach (var module in _modulesConfig.GetModules())
+            {
+                if (!module.IsBundledWithHost)
+                {
+                    TryLoadModuleAssembly(module.Id, module);
+                    if (module.Assembly == null)
+                    {
+                        throw new Exception($"Cannot find main assembly for module {module.Id}");
+                    }
+                }
+                else
+                {
+                    module.Assembly = Assembly.Load(new AssemblyName(module.Id));
+                }
+
+                GlobalConfiguration.Modules.Add(module);
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomizedMvc(this IServiceCollection services, IList<ModuleInfo> modules)
+        {
+            var mvcBuilder = services
+                .AddMvc(o =>
+                {
+                    o.EnableEndpointRouting = false;
+                });
+
+            foreach (var module in modules.Where(x => !x.IsBundledWithHost))
+            {
+                AddApplicationPart(mvcBuilder, module.Assembly);
+            }
+
+            return services;
+        }
+
+        private static void AddApplicationPart(IMvcBuilder mvcBuilder, Assembly assembly)
+        {
+            var partFactory = ApplicationPartFactory.GetApplicationPartFactory(assembly);
+            foreach (var part in partFactory.GetApplicationParts(assembly))
+            {
+                mvcBuilder.PartManager.ApplicationParts.Add(part);
+            }
+
+            var relatedAssemblies = RelatedAssemblyAttribute.GetRelatedAssemblies(assembly, throwOnError: false);
+            foreach (var relatedAssembly in relatedAssemblies)
+            {
+                partFactory = ApplicationPartFactory.GetApplicationPartFactory(relatedAssembly);
+                foreach (var part in partFactory.GetApplicationParts(relatedAssembly))
+                {
+                    mvcBuilder.PartManager.ApplicationParts.Add(part);
+                }
+            }
+        }
+
+        private static void TryLoadModuleAssembly(string moduleFolderPath, ModuleInfo module)
+        {
+            const string binariesFolderName = "bin";
+            var binariesFolderPath = Path.Combine(moduleFolderPath, binariesFolderName);
+            var binariesFolder = new DirectoryInfo(binariesFolderPath);
+
+            if (Directory.Exists(binariesFolderPath))
+            {
+                foreach (var file in binariesFolder.GetFileSystemInfos("*.dll", SearchOption.AllDirectories))
+                {
+                    Assembly assembly;
+                    try
+                    {
+                        assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+                    }
+                    catch (FileLoadException)
+                    {
+                        // Get loaded assembly. This assembly might be loaded
+                        assembly = Assembly.Load(new AssemblyName(Path.GetFileNameWithoutExtension(file.Name)));
+
+                        if (assembly == null)
+                        {
+                            throw;
+                        }
+
+                        string loadedAssemblyVersion = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+                        string tryToLoadAssemblyVersion = FileVersionInfo.GetVersionInfo(file.FullName).FileVersion;
+
+                        // Or log the exception somewhere and don't add the module to list so that it will not be initialized
+                        if (tryToLoadAssemblyVersion != loadedAssemblyVersion)
+                        {
+                            throw new Exception($"Cannot load {file.FullName} {tryToLoadAssemblyVersion} because {assembly.Location} {loadedAssemblyVersion} has been loaded");
+                        }
+                    }
+
+                    if (Path.GetFileNameWithoutExtension(assembly.ManifestModule.Name) == module.Id)
+                    {
+                        module.Assembly = assembly;
+                    }
+                }
+            }
+        }
+    }
+}
