3918525c77d49b1affefc385f6899ffd3f13e006
Minh Hiền <minhhiena712@gmail.com>
2022-02-08 17:08:24 +0700

    init

---
+++ src/Demo.Infrastructure/GlobalConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Infrastructure.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Infrastructure
+{
+    public static class GlobalConfiguration
+    {
+        public static IList<ModuleInfo> Modules { get; set; } = new List<ModuleInfo>();
+        public static string WebRootPath { get; set; }
+
+        public static string ContentRootPath { get; set; }
+    }
+}
