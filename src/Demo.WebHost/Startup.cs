8aef2831c7b141f33d7cafb39bf2ef0622068560
Minh Hiền <minhhiena712@gmail.com>
2022-02-08 17:12:08 +0700

    init

--- src/Demo.WebHost/Startup.cs
+++ src/Demo.WebHost/Startup.cs
@@ -1,5 +1,9 @@
+using Demo.Infrastructure;
+using Demo.Infrastructure.Modules;
+using Demo.WebHost.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,17 +16,37 @@ namespace Demo.WebHost
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+        {            
+            GlobalConfiguration.WebRootPath = _hostingEnvironment.WebRootPath;
+            GlobalConfiguration.ContentRootPath = _hostingEnvironment.ContentRootPath;
+            services.AddModules(_hostingEnvironment.ContentRootPath);
+
+            services.AddCustomizedMvc(GlobalConfiguration.Modules);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new ModuleViewLocationExpander());
+            });
+
+            var sp = services.BuildServiceProvider();
+
+            var moduleInitializers = sp.GetServices<IModuleInitializer>();
+            foreach (var moduleInitializer in moduleInitializers)
+            {
+                moduleInitializer.ConfigureServices(services);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,18 +60,41 @@ namespace Demo.WebHost
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //    app.UseEndpoints(endpoints =>
+            //    {
+            //        endpoints.MapControllerRoute(
+            //            name: "default",
+            //            pattern: "{module=module-1}/{controller=Home}/{action=Index}/{id?}");
+            //    });
+            //});
+
+            //// Adds endpoints defined in modules
+            //var modules = app.ApplicationServices.GetRequiredService<IEnumerable<Module>>();
+            //foreach (var module in modules)
+            //{
+            //    app.Map($"/{module.RoutePrefix}", builder =>
+            //    {
+            //        builder.UseRouting();
+            //        module.Startup.Configure(builder, env);
+            //    });
+            //}
+            app.UseCustomizedStaticFiles(env);
+            app.UseCustomizedMvc();
+
+            var moduleInitializers = app.ApplicationServices.GetServices<IModuleInitializer>();
+            foreach (var moduleInitializer in moduleInitializers)
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+                moduleInitializer.Configure(app, env);
+            }
         }
     }
 }
