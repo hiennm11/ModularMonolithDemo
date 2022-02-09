137d5446af6a7e7f7ef0ac03f28e2a367b7d0455
Minh Hiền <minhhiena712@gmail.com>
2022-02-09 11:21:41 +0700

    add view component extension to get viewcomponent path in orther module to render to page

---
+++ src/Demo.Infrastructure/Extensions/ViewComponentExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Demo.Infrastructure.Extensions
+{
+    public static class ViewComponentExtension
+    {
+        public static string GetViewPath(this ViewComponent viewComponent, string viewName = "Default", bool isViewComponentInArea = false)
+        {
+            var moduleName = viewComponent.GetType().Assembly.GetName().Name.Split('.').Last();
+            var componentShortName = viewComponent.ViewComponentContext.ViewComponentDescriptor.ShortName;
+
+            string viewPath = $"/Views/Shared/Components/{componentShortName}/{viewName}.cshtml";
+
+            if (isViewComponentInArea)
+            {
+                viewPath = $"/Areas/{moduleName}" + viewPath;
+            }          
+
+            return viewPath;
+        }
+    }
+}
---
+++ src/Modules/Demo.Modules.Module2/Views/Shared/Components/Demo/DemoViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Demo.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Modules.Module2.Views.Shared.Components.Demo
+{
+    public class DemoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(this.GetViewPath());
+        }
+    }
+}
